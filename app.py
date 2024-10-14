d630cbd5e9cda1e092f07eafd84aedd4a63c5f59
Manasi Athalye <108363698+manasi190@users.noreply.github.com>
2024-10-14 16:52:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ env_var = os.getenv('ENV_VAR')
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'<h1>Environment Variable : {env_var}</h1>'
 
 if __name__ == '__main__':
     app.run()
